52892623febde535f6740d1743a1e36d9c5f76ce
Alvar Wegelius <alvar.wegelius@almamedia.fi>
2018-10-23 16:56:16 +0300

    First layout implementation

--- frontend/src/components/landingPage/landingPage.js
+++ frontend/src/components/landingPage/landingPage.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import getList from './mockdata';
 import './landingPage.scss';
 
 export default class LandingPage extends Component {
 
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      loggedIn: false,
+    };
+    this.testList = getList();
+  }
+
+
   loginClick = () => {
-    console.log('Log in clicked');
+    this.setState({
+      loggedIn: !this.state.loggedIn,
+    });
   }
   render() {
+    const { loggedIn = false } = this.state;
     return (
       <div>
         <div className="landing-page-header-container">
@@ -22,7 +36,28 @@ export default class LandingPage extends Component {
             </p>
           </div>
         </div>
-        
+        <div className="landing-page-filters">
+          <div className="landing-page-filter-names">
+            <p> Nearby </p>
+          </div>
+          { loggedIn &&
+          <div className="landing-page-filter-names">
+            <p> Favourites </p>
+          </div>
+          }
+          { loggedIn &&
+          <div className="landing-page-filter-names">
+            <p> History </p>
+          </div>
+          }
+          { loggedIn &&
+          <div className="landing-page-filter-names">
+            <p> Admin tools </p>
+          </div>
+          }
+        </div>
+        <div className="landing-page-store-list-container">
+        </div>
       </div>
     );
   }
---
+++ frontend/src/components/landingPage/mockdata.js
@@ -0,0 +1,8 @@
+export default function getList() {
+  return [
+    { name: 'prisma', distance: 100 },
+    { name: 'alepa', distance: 13 },
+    { name: 'alko', distance: 3 },
+    { name: 'k-kauppa', distance: 303 }
+  ]; 
+}
