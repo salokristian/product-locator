41e8f3a0baecb4ce18a8ed1e0d6b058ebd11c7ed
Toikkanen Teemu <teemu.toikkanen@aalto.fi>
2018-10-23 21:22:14 +0300

    testiä kotikoneella, suunnittelua..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import LandingPage from './components/landingPage/landingPage'
+import MapPage from './components/MapPage/MapPage'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        {/* <LandingPage /> */}
+        <MapPage />
       </div>
     );
   }
--- frontend/src/components/mapPage/mapPage.js
+++ frontend/src/components/mapPage/mapPage.js
@@ -1,4 +1,22 @@
 import React, { Component } from 'react';
 
+export default class MapPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // TODO: mitä state-infoa tarvitsee? tallennanko ylimpään komponenttiin mahd.paljon?
+      // Joo? Eli tähän? Siis ainakin jo etsityt tuotteet mistä pingi kartalla.
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div>search bar</div>
 
+        <div>kartta</div>
+      </div>
+    );
+  }
+}
 
+// Muita (statless function()?) tai classeja? esim. MapPage ainoa missä state, lisäksi function StoreLayout ja function SearchBar ?
