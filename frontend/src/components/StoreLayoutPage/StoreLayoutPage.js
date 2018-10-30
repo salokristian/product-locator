c4c0b4baf1ea70feef359ea025ff1e22dba5031f
Teemu Toikkanen <teemu.toikkanen@aalto.fi>
2018-10-30 14:41:54 +0200

    searchbar, ei toimi viela

--- frontend/src/components/StoreLayoutPage/StoreLayoutPage.js
+++ frontend/src/components/StoreLayoutPage/StoreLayoutPage.js
@@ -1,6 +1,98 @@
 import React, { Component } from 'react';
 import './StoreLayoutPage.scss';
-// import backendApi from 'backendApi';
+import Autosuggest from 'react-autosuggest';
+
+// Imagine you have a list of languages that you'd like to autosuggest.
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  
+];
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : languages.filter(lang =>
+    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name}
+  </div>
+);
+
+export class SearchBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '',
+      suggestions: []
+    };
+  }
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+   // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above (???), so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    console.log("onSuggestionsFetchRequested");
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  render() {
+    const {value, suggestions} = this.state;
+
+    const inputProps = {
+      placeholder: 'Type a product',
+      value,
+      onChange: this.onChange
+    };
+
+    return(
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+    );
+
+  }
+
+}
+
 
 export default class StoreLayoutPage extends Component {
   constructor(props) {
@@ -68,7 +160,7 @@ export default class StoreLayoutPage extends Component {
       svg += 'style="stroke:rgb(0,0,0);" />\n';
     }
 
-    //shelves
+    //svg shelves
 
     var shelves = floorsShelvesJson['shelves'];
 
@@ -98,7 +190,7 @@ export default class StoreLayoutPage extends Component {
     return (
       <div className="store-layout-page">
         <div className="product-search-bar">
-        search bar
+        <SearchBar/>
         </div>
         <div className="store-layout">
           {this.getSvg()}
@@ -110,14 +202,14 @@ export default class StoreLayoutPage extends Component {
 
 
 
-// ProductSearchBar
-export class ProductSearchBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-}
+// // ProductSearchBar
+// export class ProductSearchBar extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     };
+//   }
+// }
 
 
 // StoreLayout
