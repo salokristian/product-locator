91663a980d74a0094e0b2a931c80683f1b4683be
Kristian Salo <salo.kristian@hotmail.com>
2018-11-05 16:58:51 +0200

    BE feat: Add all models to admin interface.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .forms import FloorForm
+from .models import (Store, Location, Shelf, Floor, Product, ProductInfo, ProductType,
+                     ShoppingList)
+
+
+class FloorAdmin(admin.ModelAdmin):
+    form = FloorForm
+
+
+admin.site.register(Store)
+admin.site.register(Location)
+admin.site.register(Floor, FloorAdmin)
+admin.site.register(Shelf)
+admin.site.register(Product)
+admin.site.register(ProductInfo)
+admin.site.register(ProductType)
+admin.site.register(ShoppingList)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Customer, Employee
 
 
 class CustomUserAdmin(UserAdmin):
@@ -16,3 +16,5 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Employee)
+admin.site.register(Customer)
