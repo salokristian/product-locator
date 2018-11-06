fb3b278f3f9e1a420f3924394689173b87f99205
Kristian Salo <salo.kristian@hotmail.com>
2018-11-06 10:37:17 +0200

    BE fix: Add widget for through model field  in Product info admin.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -9,11 +9,24 @@ class FloorAdmin(admin.ModelAdmin):
     form = FloorForm
 
 
+class ProductInline(admin.TabularInline):
+    """
+    An inline model is needed because django doesn't display admin widgets
+    by default when a through model is used.
+    """
+    model = Product
+    extra = 1
+
+
+class ProductInfoAdmin(admin.ModelAdmin):
+    inlines = (ProductInline,)
+
+
 admin.site.register(Store)
 admin.site.register(Location)
 admin.site.register(Floor, FloorAdmin)
 admin.site.register(Shelf)
 admin.site.register(Product)
-admin.site.register(ProductInfo)
+admin.site.register(ProductInfo, ProductInfoAdmin)
 admin.site.register(ProductType)
 admin.site.register(ShoppingList)
