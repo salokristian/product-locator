a5b0a08fa83bde5d9d780109cbecfc0d6aa47dd1
Kristian Salo <salo.kristian@hotmail.com>
2018-11-11 19:31:22 +0200

    BE feat: Add JWT authentication.- create endpoint /token for retrieving JWT with username and password- the JWT needs to be included in Authorization header with Bearerprefix for endpoints requiring authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from api import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
+    path('token', obtain_jwt_token),
     path('stores', views.StoreList.as_view()),
     path('stores/<int:pk>', views.StoreDetail.as_view()),
     path('stores/<int:pk>/products', views.ProductList.as_view())
--- backend/product_locator/settings.py
+++ backend/product_locator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,7 +141,22 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',  # TODO: Remove in production
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     )
+}
+
+JWT_AUTH = {
+    'JWT_RESPONSE_PAYLOAD_HANDLER': 'utils.jwt.jwt_response_handler',
+    'JWT_SECRET_KEY': SECRET_KEY,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=30),
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+    'JWT_AUTH_COOKIE': None,
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
