7234a1f8119bddea0d932be43c0e5320f4fb12d5
Kristian Salo <salo.kristian@hotmail.com>
2018-11-19 14:12:24 +0200

    BE feat: Serve react frontend from django.

--- backend/product_locator/settings.py
+++ backend/product_locator/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'product_locator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,7 +130,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 
