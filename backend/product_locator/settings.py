8505a52d669c37b34aea3c994e2746be9a990cb1
Kristian Salo <salo.kristian@hotmail.com>
2018-11-19 14:30:46 +0200

    Change debug to false for deployed version.

--- backend/product_locator/settings.py
+++ backend/product_locator/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qolvk)nqyee*7ux8@$676)ppc@r5!ks=&i!l_+w9=e!8leezqq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
